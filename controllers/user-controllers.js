1953641a8ed2bf8e70d68640f90ec9ee5fc43dfd
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-29 22:48:40 +0545

    feat: added user controllers

---
+++ controllers/user-controllers.js
@@ -0,0 +1,23 @@
+const User = require('../models/user-model');
+
+exports.registerUser = async (req, res) => {
+    try {
+
+        const user = await new User(req.body);
+
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+
+exports.getAllUsers = async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).send(users);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
