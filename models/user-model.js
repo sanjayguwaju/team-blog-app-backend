a5a96a78484496f782f22d88b757e691e5b34bd4
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-29 22:49:22 +0545

    feat: userSchema added

---
+++ models/user-model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}); // Rules for the data we want to store
+
+const User = mongoose.model('User', userSchema);  //users = collection banxa
+
+module.exports = User;
