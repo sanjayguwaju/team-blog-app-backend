651b575b29255a4efe7b83a189c064e84ba8cccc
Kushal Bajracharya <bajrakus20@gmail.com>
2023-08-30 18:11:45 +0545

    created new update user controller

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -21,3 +21,20 @@ exports.getAllUsers = async (req, res) => {
       res.status(500).send(error);
     }
   };
+
+  exports.updateUser = async (req, res) => {
+    try{
+      const userId = req.params.id;
+      const updatedData = req.body;
+
+    const user = await User.findByIdAndUpdate(userId, updatedData, { new: true });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating user'});
+  }
+};
