4002e0ab181e6fd17cebc102a45010f4074ba462
smriteemanandhar <smriteemanandhar@gmail.com>
2023-08-31 00:02:35 +0545

    fixed: typo issue in user controller

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -56,14 +56,14 @@ exports.getAllUsers = async (req, res) => {
       const userId = req.params.id;
       const updatedData = req.body;
 
-    const user = await User.findByIdAndUpdate(userId, updatedData, { new: true });
+    const user = await User.findByIdAndUpdate(userId, updatedData);
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json(users);
+    res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ message: 'Error updating user'});
+    res.status(500).json({ message: 'Error updating smritee user'});
   }
 };
