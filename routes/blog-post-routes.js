3d4690ece905a0198a30b557d45c55b05bfe081a
sanjayguwaju <sanjayguwaju@gmail.com>
2023-09-30 17:50:44 +0545

    fix: no authorization for now to the blog route

--- routes/blog-post-routes.js
+++ routes/blog-post-routes.js
@@ -4,9 +4,9 @@ const blogPostController = require('../controllers/blog-post-controller');
 const authorization = require('../middlewares/auth-middleware');
 
 blogRouter.post('/createblog', blogPostController.createBlogPost);
-blogRouter.put('/updateblog/:id', authorization.verifyAccessToken, blogPostController.updateBlogPost);
+blogRouter.put('/updateblog/:id',blogPostController.updateBlogPost);
 blogRouter.get('/getallblog', blogPostController.getAllBlogPost);
-blogRouter.delete('/deleteblog/:id', authorization.verifyAccessToken, blogPostController.deleteBlogPost);
+blogRouter.delete('/deleteblog/:id',blogPostController.deleteBlogPost);
 blogRouter.get('/getblogpostbyid/:id', blogPostController.getblogPostById);
 
 module.exports = blogRouter;
