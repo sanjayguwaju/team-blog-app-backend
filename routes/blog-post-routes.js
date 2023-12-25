e21ad7f83239c632d8573ee5836e522db560d4f9
sanjayguwaju <sanjayguwaju@gmail.com>
2023-12-25 08:04:16 +0545

    fix: removed the unwanted swagger comments

--- routes/blog-post-routes.js
+++ routes/blog-post-routes.js
@@ -9,24 +9,6 @@ blogRouter.get('/getallblog', blogPostController.getAllBlogPost);
 blogRouter.delete('/deleteblog/:id',blogPostController.deleteBlogPost);
 blogRouter.get('/getblogpostbyid/:id', blogPostController.getblogPostById);
 blogRouter.get('/get-blog-post-by-user-id/:id', blogPostController.getBlogPostsByUserId);
-
-/**
- * @swagger
- * /blogs/search/{query}:
- *   get:
- *     summary: Search blog posts
- *     parameters:
- *       - in: path
- *         name: query
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Successfully retrieved blog posts
- *       400:
- *         description: Error retrieving blog posts
- */
 blogRouter.get('/search', blogPostController.searchBlogPosts);
 
 module.exports = blogRouter;
